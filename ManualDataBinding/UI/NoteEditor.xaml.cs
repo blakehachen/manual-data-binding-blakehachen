cd16d0ee7c6c11c8efd4b053c07dc48e81c51b5e
Blake Hachen <blakehachen@ksu.edu>
2020-03-18 21:01:19 -0500

    Added two way data binding for text editor

--- ManualDataBinding/UI/NoteEditor.xaml.cs
+++ ManualDataBinding/UI/NoteEditor.xaml.cs
@@ -19,9 +19,60 @@ namespace ManualDataBinding.UI
     /// </summary>
     public partial class NoteEditor : UserControl
     {
+        private Note note;
+        /// <summary>
+        /// The Note that will be edited by this control
+        /// </summary>
+        public Note Note
+        {
+            get { return note; }
+            set
+            {
+                if (note != null) note.NoteChanged -= OnNoteChange;
+                note = value;
+                if (note != null)
+                { 
+                    note.NoteChanged += OnNoteChange;
+                    OnNoteChange(note, new EventArgs());
+                }
+            }
+        }
+
         public NoteEditor()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// This function checks if the text within the note is changed it will grab from the Title and Body properties and set the text to user input
+        /// </summary>
+        /// <param name="sender">Text inputted by user</param>
+        /// <param name="e">the event args</param>
+        public void OnNoteChange(object sender, EventArgs e)
+        {
+            if (Note == null) return;
+            Title.Text = Note.Title;
+            Body.Text = Note.Body;
+        }
+
+        /// <summary>
+        /// Needed for two way data binding this will set the title property of the Note to the text property of the label.
+        /// </summary>
+        /// <param name="sender">the button clicked</param>
+        /// <param name="e">the event args for text changing</param>
+        public void OnTitleChanged(object sender, TextChangedEventArgs e)
+        {
+            Note.Title = Title.Text;
+        }
+
+        /// <summary>
+        /// Needed for two way data binding this will set the body property of the Note to the text property of the textbox.
+        /// </summary>
+        /// <param name="sender">text inserted</param>
+        /// <param name="e">the event args for text changing</param>
+        public void OnBodyChanged(object sender, TextChangedEventArgs e)
+        {
+            Note.Body = Body.Text;
         }
     }
 }
